3e74c177b21cb9e5e93ba3fed4d73fdfbd0673d4
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2023-02-07 11:21:38 -0300

    image folder

--- app/Http/Controllers/CKEditorController.php
+++ app/Http/Controllers/CKEditorController.php
@@ -13,10 +13,10 @@ class CKEditorController extends Controller
             $fileName = pathinfo($originName, PATHINFO_FILENAME);
             $extension = $request->file('upload')->getClientOriginalExtension();
             $fileName = $fileName.'_'.time().'.'.$extension;
-            $request->file('upload')->move(public_path('img'), $fileName);
+            $request->file('upload')->move(public_path('images'), $fileName);
             $CKEditorFuncNum = $request->input('CKEditorFuncNum');
-            $url = asset('img/'.$fileName);
-            $msg = 'Image successfully uploaded';
+            $url = asset('images/'.$fileName);
+            $msg = 'Imagem foi salva no servidor!!!';
             $response = "<script>window.parent.CKEDITOR.tools.callFunction($CKEditorFuncNum, '$url', '$msg')</script>";
 
             @header('Content-type: text/html; charset=utf-8');
